a5eef0186496937e5c3267f07a87423b9c3f07e1
Luis Manuel Torres Trevino <luis.trevino@drumbot.com.mx>
2021-04-03 01:15:49 -0600

    Add app/__init__.py file
    Se agrego el archivo que inicia el paquete de la aplicacion flask

---
+++ app/__init__.py
@@ -0,0 +1,13 @@
+"""
+author: Luis Manuel Torres Trevino
+description: Este archivo crea toda la aplicacion de flask
+"""
+import os
+
+from flask import Flask 
+from flask_pymongo import PyMongo
+
+app = Flask(__name__)
+app.config.from_object('config.Config')
+
+mongo = PyMongo(app)
