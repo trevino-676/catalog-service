2c9c47e711129c1181524639f9295c287f822467
Luis Manuel Torres Trevino <luis.trevino@drumbot.com.mx>
2021-04-03 12:40:11 -0600

    Se agregaron nuevos metodos a la clase DTO

--- app/model/dto.py
+++ app/model/dto.py
@@ -3,14 +3,29 @@ author: Luis Manuel Torres Trevino
 description: Este archivo contiene la clase base para todos los
     objetos dto del microservicio
 """
-from abc import abstractmethod, ABC
+from bson import ObjectId
 
-class DTO(ABC):
 
-    @abstractmethod
+class DTO(dict):
+    __getattr__ = dict.get
+    __delattr__ = dict.__delattr__
+    __setattr__ = dict.__setattr__
+
     def save(self):
-        pass
+        if not self._id:
+            self.collection.insert_one(self)
+        else:
+            self.collection.replace_one({"_id": ObjectId(self._id)}, self)
 
-    @abstractmethod
-    def to_dict(self):
-        pass
+    def reload(self):
+        if self._id:
+            self.update(self.collection.find_one({"_id": ObjectId(self.__id)}))
+
+    def remove(self):
+        if self._id:
+            self.collection.delete_one({"_id": ObjectId(self._id)})
+            self.clear()
+
+    def find(self, filters: dict):
+        if dict is not None:
+            self.update(self.collection.find_one(filters))
