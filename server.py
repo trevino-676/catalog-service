103fd056350825112d3625f8be418029517bdcf1
Luis Manuel Torres Trevino <luis.trevino@drumbot.com.mx>
2021-04-03 01:16:21 -0600

    Add server.py file
    Se agrego el archivo principal de la aplicacion flask

---
+++ server.py
@@ -0,0 +1,8 @@
+"""
+author: Luis Manuel Torres Trevino
+description: Este archivo es el principal para la aplicacion flask
+"""
+from app import app
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=5000)
