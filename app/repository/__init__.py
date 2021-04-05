aabbd0a7eeb449cbfb934848ceaf87b1c9a35fd7
Luis Manuel Torres Trevino <luis.trevino@drumbot.com.mx>
2021-04-05 00:47:09 -0500

    Se agrega la instancia de repository en el __init__

--- app/repository/__init__.py
+++ app/repository/__init__.py
@@ -1,1 +1,4 @@
-from app.repository.repository import Repository 
+from app.repository.repository import Repository
+from app.repository.user_repository import UserMongoRepository
+
+repository = UserMongoRepository()
