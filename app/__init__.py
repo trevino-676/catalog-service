1f7f3521e75d05de35c5c3c95f33b8d1f74cd915
Luis Manuel Torres Trevino <luis.trevino@drumbot.com.mx>
2021-04-08 23:19:22 -0500

    Se creo una instancia del repositorio UserMongoRepository

--- app/__init__.py
+++ app/__init__.py
@@ -4,10 +4,10 @@ description: Este archivo crea toda la aplicacion de flask
 """
 import os
 
-from flask import Flask 
+from flask import Flask
 from flask_pymongo import PyMongo
 
 app = Flask(__name__)
-app.config.from_object('config.Config')
+app.config.from_object("config.Config")
 
 mongo = PyMongo(app)
--- app/repository/__init__.py
+++ app/repository/__init__.py
@@ -1,4 +1,4 @@
 from app.repository.repository import Repository
 from app.repository.user_repository import UserMongoRepository
 
-repository = UserMongoRepository()
+urepository = UserMongoRepository()
