e6814ab4225b599d4d46d7495ca754e9b923bcc4
Luis Manuel Torres Trevino <luis.trevino@drumbot.com.mx>
2021-04-10 13:55:13 -0500

    Se corrigue un problema con el nombre del archvio

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ from os import path, environ
 
 from dotenv import load_dotenv
 
-BASE_DIR = path.abspath(path.dirname(__file))
+BASE_DIR = path.abspath(path.dirname(__file__))
 DOTENV_PATH = path.join(BASE_DIR, ".env")
 load_dotenv(DOTENV_PATH)
 
