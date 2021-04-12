90206237ab3d9b263147f682c7fd1f4a681123e9
Luis Manuel Torres Trevino <luis.trevino@drumbot.com.mx>
2021-04-12 00:28:48 -0500

    Se agrego un nuevo metodo para parsear los diccionarios a json.

--- app/routes.py
+++ app/routes.py
@@ -3,8 +3,8 @@ author: Luis Manuel Torres Trevino
 description: Este archivo contiene las rutas de los microservicios
 """
 from flask import Blueprint, make_response, jsonify, request
+from bson.json_util import dumps
 
-from app import app
 from app.service import user_service
 from app.utils import validate_user, FilterType, make_filters
 
@@ -18,13 +18,13 @@ def get_users():
     el sistema
     """
     users = user_service.get_users(make_filters(FilterType.AND, request.json))
-    response = {}
     if not users:
         response = {"status": False, "users": []}
         return make_response(jsonify(response), 500)
 
     response = {"status": True, "users": users}
-    return make_response(jsonify(response), 200)
+
+    return make_response(dumps(response), 200)
 
 
 @user_routes.route("/", methods=["POST"])
@@ -37,7 +37,7 @@ def save_user():
     if len(missing_fields) > 0:
         response = {
             "status": False, 
-            "message": f"Faltan los siguientes campos: " + [field for field in missing_fields]
+            "message": f"Faltan los siguientes campos: {str(map(lambda field : field, missing_fields))}"
         }
         return make_response(jsonify(response), 404)
     
@@ -50,7 +50,7 @@ def save_user():
     
     response = {
         "status": True,
-        "id": str(user_service.get_user({"$and": [{"name": user["name"]}, {"email": user["email"]}]})._id)
+        "id": "Se guardo correctamente el usuario"
     }
     return make_response(jsonify(response), 200)
 
@@ -69,12 +69,11 @@ def get_user():
             "message": "No se encontro al usuario que intentas buscar"
         }
         return make_response(jsonify(response), 404)
-    
     response = {
         "status": True,
         "user": user
     }
-    return make_response(jsonify(response), 200)
+    return make_response(dumps(response), 200)
 
 
 @user_routes.route("/", methods=["PUT"])
@@ -86,12 +85,12 @@ def update_user():
     if not user_service.update_user(user):
         response = {
             "status": False, 
-            "message": f"No se pudo actualizar el usuario: {user._id}"
+            "message": f"No se pudo actualizar el usuario: {str(user._id)}"
         }
         return make_response(jsonify(response), 404)
     response = {
         "status": True,
-        "message": f"Se actualizo corretamente el usuario: {user._id}"
+        "message": f"Se actualizo corretamente el usuario: {str(user._id)}"
     }
     return make_response(jsonify(response), 200)
 
@@ -105,12 +104,12 @@ def delete_user():
     if user_service.delete_user(user_id) != user_id:
         response = {
             "status": False, 
-            "message": f"No se pudo eliminar el usuario: {user_id}"
+            "message": f"No se pudo eliminar el usuario: {str(user_id)}"
         }
         return make_response(jsonify(response), 404)
     response = {
         "status": True,
-        "message": f"Se elimino corretamente el usuario: {user_id}"
+        "message": f"Se elimino corretamente el usuario: {str(user_id)}"
     }
     return make_response(jsonify(response), 200)
 
