3d730dd454de130960b3faf111fab9e8e694a8cb
Luis Manuel Torres Trevino <luis.trevino@drumbot.com.mx>
2021-09-10 22:45:25 -0500

    Se cambio la ip en la que corre la api

--- main.py
+++ main.py
@@ -4,5 +4,5 @@ description: Este archivo es el principal para la aplicacion flask
 """
 from app import app
 
-if __name__ == '__main__':
-    app.run(host="127.0.0.1", port=5000)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
